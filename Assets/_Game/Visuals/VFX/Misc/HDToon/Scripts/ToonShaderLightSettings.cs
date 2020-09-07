f2db1ef858366f1c167317fde5114786ccbbb43c
Cameron <cameron.meyer02@gmail.com>
2020-09-07 11:21:58 -0500

    Toon Shader Testing Before Merge

---
+++ Assets/_Game/Visuals/VFX/Misc/HDToon/Scripts/ToonShaderLightSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ToonShaderLightSettings : MonoBehaviour
+{
+
+	private Light mainLight;
+
+	void OnEnable()
+	{
+		mainLight = GetComponent<Light>();
+	}
+	
+	void Update ()
+	{
+		Shader.SetGlobalVector("_ToonLightDirection", -transform.forward);
+		Shader.SetGlobalColor("_ToonLightColor", mainLight.color);
+        Shader.SetGlobalFloat("_ToonLightIntensity", mainLight.intensity);
+	}
+}
