38434af9ab18d067e3558bdc2e478beb06802170
JeMarra Rivers <jmonetrivers1@gmail.com>
2020-09-08 17:11:29 -0500

    Room instantiation in proper world position
    Test Rooms are instantiating in proper locations (Entrance to exit) but auto rotation needs to be implemented

--- Assets/_Game/Scripts/Mechanics/Level Generation/LevelGenTest.cs
+++ Assets/_Game/Scripts/Mechanics/Level Generation/LevelGenTest.cs
@@ -18,30 +18,32 @@ public class LevelGenTest : MonoBehaviour
     [SerializeField]
     private float desiredLevelDifficulty; 
     public float DesiredLevelDifficulty { get => desiredLevelDifficulty; set => desiredLevelDifficulty = value; }
-    [Tooltip("The difficulty at end of level. DO NOT EDIT")]
     [SerializeField]
+    [Tooltip("The difficulty at end of level. DO NOT EDIT")]
     private float currentLevelDifficulty = 0; 
     public float CurrentLevelDifficulty { get => currentLevelDifficulty; set => currentLevelDifficulty = value; }
+    [SerializeField]
+    private GameObject dropShipRoom;
+    public GameObject DropShipRoom { get => dropShipRoom; set => dropShipRoom = value; }
 
     //Variables to detect Entrance/Exit Rotations
+    public Vector3 currentExitLocation;
     private float priorRoomExitRotation = 0;    //default should = dropship room exit rotation
     private float currentLevelRotation = 0;     //to prevent room spiraling with angular arrangements
-
+    private Quaternion priorRoomRotation;
 
     void Start() //on scene start, generate level
     {
         ShuffleRoomList(roomPrefabs);       //randomizes room array
-
+        currentExitLocation = dropShipRoom.GetComponent<Room>().Exit.transform.TransformPoint(Vector3.zero);
+        priorRoomRotation = dropShipRoom.GetComponent<Room>().Exit.rotation;
         /*
         while(currentLevelDifficulty < desiredLevelDifficulty)
         {
             //call function
-            currentLevelDifficulty += 1; //placeholder so while loop breaks
         }
         */
-
     }
-
 
     private void Update()
     {
@@ -51,7 +53,10 @@ public class LevelGenTest : MonoBehaviour
         }
     }
 
-    //shuffle a list
+
+    /// <summary>
+    /// This will randomize the roomList on start (shuffle the deck)
+    /// </summary>
     private void ShuffleRoomList(List<GameObject> roomList)
     {
         Debug.Log("Random List: ");
@@ -63,24 +68,63 @@ public class LevelGenTest : MonoBehaviour
             roomList[randomNum] = placeHolder;
         }
     }
+
+
+    /// <summary>
+    /// This fucntion will take in the the randomized roomList and instantiate the first room if it passes check.
+    /// Room is removed from list when instantiated.
+    /// Checks:
+    ///     Rotation
+    /// </summary>
     private void InstantiateValidRoom(List<GameObject> roomList)
     {
         GameObject toRemove = null;
-        for (int i = 0; i < roomList.Count; i++) //goes through list until a break
+        float blank = 0;
+        for (int i = 0; i < roomList.Count; i++) //goes through list until a break (will break on chosen room/continues on failed room check)
         {
-            toRemove = roomList[i];
-            Instantiate(roomList[i]);
-            break;
-
+            if (blank == 0){        //will be room rotation check
+                toRemove = roomList[i];
+                GameObject plz = Instantiate(roomList[i], currentExitLocation + FixTransformDeficit(roomList[i]), priorRoomRotation);
+                //plz.transform.RotateAround(currentExitLocation,Vector3.up, RotateRoom(priorRoomRotation, roomList[i]));
+                priorRoomRotation = plz.transform.rotation; //change to of exit
+                //plz.transform.position = currentExitLocation + FixTransformDeficit(roomList[i]);
+                currentExitLocation = plz.GetComponent<Room>().Exit.transform.TransformPoint(Vector3.zero);
+                break;
+            }
             //check room rotation
                 //good --> go on; bad--> continue
         }
-        Debug.Log("Removed Object: " + toRemove);
+        //Debug.Log("Removed Object: " + toRemove);
         roomList.Remove(toRemove);
-        Debug.Log("Removed.");
+    }
 
-
+    private Vector3 FixTransformDeficit(GameObject genericRoom)
+    {
+        return (genericRoom.transform.position - genericRoom.GetComponent<Room>().Entrance.transform.position);
     }
 
 
+    private float RotateRoom(Quaternion previousRoom,GameObject genericRoom)
+    {
+        Quaternion adjustmentRotation = new Quaternion(0,0,0,0);
+        //Debug.Log("Previous_rot: " + previousRoom.eulerAngles + " NewRoom_rot " + genericRoom.GetComponent<Room>().Entrance.transform.rotation.eulerAngles);
+        //float angleDifferenceFloat = Quaternion.Angle(previousRoom, genericRoom.GetComponent<Room>().Entrance.transform.rotation);
+        float neededRotate = 0; //the y rotation needed to match
+        //Debug.Log("Angle difference: " + angleDifferenceFloat);
+
+
+        if (genericRoom.GetComponent<Room>().Entrance.transform.rotation.eulerAngles.y > 180)
+        {
+            neededRotate = (360 - genericRoom.GetComponent<Room>().Entrance.transform.rotation.eulerAngles.y);
+            Debug.Log("Correction: " + neededRotate);
+        }
+        else if (genericRoom.GetComponent<Room>().Entrance.transform.rotation.eulerAngles.y <= 180)
+        {
+            neededRotate = -(genericRoom.GetComponent<Room>().Entrance.transform.rotation.eulerAngles.y);
+            Debug.Log("Correction: " + neededRotate);
+        }
+        //Debug.Log("New Rotation: " + (adjustmentRotation.eulerAngles = new Vector3(0, neededRotate, 0)));
+        //Debug.Log("Would be new rotation");
+        return neededRotate;
+    }
 }
