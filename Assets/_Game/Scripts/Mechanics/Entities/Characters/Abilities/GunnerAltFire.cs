010f588d8b8d501cadbeca786000561f661c662e
Henry Hazlett <ticotoad@gmail.com>
2020-09-08 22:53:12 -0500

    Create Gunner and Ability classes

---
+++ Assets/_Game/Scripts/Mechanics/Entities/Characters/Abilities/GunnerAltFire.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunnerAltFire : Ability
+{
+    protected override void ActivateAbility()
+    {
+
+    }
+}
---
+++ Assets/_Game/Scripts/Mechanics/Entities/Characters/Abilities/GunnerPrimaryFire.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunnerPrimaryFire : Ability
+{
+    protected override void ActivateAbility()
+    {
+
+    }
+}
