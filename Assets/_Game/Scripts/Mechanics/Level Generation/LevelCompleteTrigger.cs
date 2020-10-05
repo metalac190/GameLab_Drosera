4ea50938b9a335a5efd2a720166038306df82673
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-10-05 15:22:40 -0500

    Level complete trigger, Game won and lost functions

---
+++ Assets/_Game/Scripts/Mechanics/Level Generation/LevelCompleteTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCompleteTrigger : MonoBehaviour
+{
+    [SerializeField] LevelGeneration levelGen;
+
+    private void Start()
+    {
+        if(!levelGen)
+            levelGen = FindObjectOfType<LevelGeneration>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerBase>() && GameManager.Instance.GameState == DroseraGlobalEnums.GameState.MainTwo)
+        {
+            print("Player completed level: " + levelGen.LevelNumber);
+            if(levelGen.LevelNumber >= 6)
+            {
+                GameManager.Instance.GameWon();
+            }
+            else
+            {
+                GameManager.Instance.LevelComplete();
+            }
+        }
+    }
+}
