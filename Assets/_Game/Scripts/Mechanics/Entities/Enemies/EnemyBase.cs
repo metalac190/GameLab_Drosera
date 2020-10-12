c1f438348669e7342440089e43e7cb3a4d76c36e
Bill Dang <sparkfire.ffs@gmail.com>
2020-10-12 13:58:56 -0500

    Fixed NavMesh errors upon loading new scene

--- Assets/_Game/Scripts/Mechanics/Entities/Enemies/EnemyBase.cs
+++ Assets/_Game/Scripts/Mechanics/Entities/Enemies/EnemyBase.cs
@@ -134,7 +134,6 @@ public abstract class EnemyBase : EntityBase {
         isHealing = false;
 
         // Change behavior
-        StopCoroutine(currentBehavior);
         currentBehavior = StartCoroutine(AggressiveMove());
         yield return null;
     }
