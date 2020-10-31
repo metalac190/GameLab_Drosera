0825bc5febf010ed0a178a29c84024c9e333afff
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-10-31 14:47:01 -0500

    Infinite ammo zone

---
+++ Assets/_Game/Scripts/Mechanics/Entities/InfiniteAmmoTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteAmmoTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Gunner player = other.GetComponent<Gunner>();
+        if(player)
+        {
+            player.SetInfiniteAmmo(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Gunner player = other.GetComponent<Gunner>();
+        if (player)
+        {
+            player.SetInfiniteAmmo(false);
+        }
+    }
+}
