c10f501b268276d10401ccdd0536cebc39adee71
Vinson Kok <vinsonkok@yahoo.com>
2020-11-14 04:00:04 -0600

    Codex Entries Setup

---
+++ Assets/_Game/UI/Scripts/Main Menu/Extras/CodexEntry1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodexEntry1 : CodexEntry
+{
+    [TextArea(0, 100)]
+    public string paragraphTextLeft;
+
+    [TextArea(0, 100)]
+    public string paragraphTextRight;
+}
