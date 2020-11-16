8a4cac485abf65dc6d4ff1e344f42ada9493f09e
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-11-15 19:56:00 -0600

    Fixed issue preventing building an exe

--- Assets/_Game/Scripts/DevConsole/ConsoleLevelEditor.cs
+++ Assets/_Game/Scripts/DevConsole/ConsoleLevelEditor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if(UNITY_EDITOR)
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -463,4 +464,4 @@ public class ConsoleLevelEditor : MonoBehaviour
     }
 }
 
-
+#endif
